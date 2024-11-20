5b1196468a522d75d04edc6c38bb537945f25983
maheshwar-divami <maheshwar@divami.com>
2024-11-21 00:21:23 +0530

    handled custom delimiter

--- src/add.ts
+++ src/add.ts
@@ -1,7 +1,58 @@
-export function add(numStr: string): number {
-  const regex = /(\\n)*[,]*/gm;
-  const numStrWithoutDelimiters = numStr.replaceAll(regex, "");
-  const numbers = numStrWithoutDelimiters.split("").map(Number);
-  const sum = numbers.reduce((acc, num) => acc + num, 0);
-  return isNaN(sum) ? 0 : sum;
+export function add(str: string): number {
+  // get custom delimiter from input string eg output: [//;\n]
+  const delimiter = str.match(/\/\/[.?;*\-+=]+\\n/gm);
+
+  // using matchAll to capture the groups eg fo this //;\n33;4 we can capture ;
+  const delimiterMatchers = delimiter
+    ? [...delimiter[0].matchAll(/\/\/(.*)\\n/gm)]
+    : null;
+
+  let delimiterVal = "";
+  if (delimiterMatchers) {
+    delimiterVal = delimiterMatchers[0][1];
+  }
+  const inputNumStr = str.slice(delimiter ? delimiter[0].length : 0);
+
+  // default delimiters
+  const delimiters = [",", "\\n"];
+
+  // adding custom delimiter to the default delimiters
+  if (delimiterVal) {
+    delimiters.push(delimiterVal);
+  }
+  // temp variable to accumulate current digits Eg: //;\n33;4 for this it stores 33
+  let currNumber = "";
+  let sum = 0;
+  for (let i = 0; i < inputNumStr.length; i++) {
+    let isDelimiter = false;
+    // find delimiter and skip the loop to the end of delimiter
+    for (let j = 0; j < delimiters.length; j++) {
+      if (delimiters[j] === inputNumStr.slice(i, delimiters[j].length + i)) {
+        isDelimiter = true;
+        i += delimiters[j].length - 1;
+        break;
+      }
+    }
+
+    // if delimiter was found add currNumber to the sum
+    if (isDelimiter && currNumber !== "") {
+      const num = Number(currNumber);
+      sum += num;
+      currNumber = "";
+    } else {
+      // if current char is not part of delimiter store it in temp variable currNumber
+      if (!isNaN(Number(inputNumStr[i]))) {
+        currNumber += inputNumStr[i];
+      } else {
+        throw new Error("Invalid input");
+      }
+    }
+  }
+  // after the last delimiter the currentNumber can be added to sum
+  if (currNumber !== "") {
+    const num = Number(currNumber);
+    sum += num;
+  }
+
+  return sum;
 }
--- test/add.test.ts
+++ test/add.test.ts
@@ -6,19 +6,28 @@ describe("String Calculator", () => {
     const result = add("2,4,5");
     expect(result).toBe(11);
   });
-  it("should return 0 if input is invalid", () => {
-    const result = add("abcd");
-    expect(result).toBe(0);
+  it("should return 0 if input empty", () => {
+    expect(add("")).toBe(0);
+  });
+  it("should throw error if input is invalid", () => {
+    expect(() => add("abcd")).toThrow("Invalid input");
   });
   it("should sum numbers with \n as delimiter", () => {
-    const result = add("2\n4\n5");
+    const result = add("2\\n4\\n5");
     expect(result).toBe(11);
   });
   it("should sum numbers with custom delimiter", () => {
-    const result = add("//;\n2");
+    const result = add("//;\\n2");
     expect(result).toBe(2);
+  });
+  it("should sum numbers with custom delimiters and default delimiters", () => {
+    expect(add("//;\\n5,6;8")).toBe(19);
+    expect(add("//;\\n5\\n6;3")).toBe(14);
   });
   it("should throw error if there are negative numbers", () => {
     expect(() => add("//;\\n2;-3;5")).toThrow("negative numbers no allowed -3");
+    expect(() => add("//;\\n2;-3;5;-6")).toThrow(
+      "negative numbers no allowed -3 -6"
+    );
   });
 });
